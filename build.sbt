6c81ea7e77d69a521bf52b068b2c6856ad993638
Noel Welsh <noel@noelwelsh.com>
2022-01-07 13:52:26 +0000

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,3 @@
+scalaVersion := "3.1.0"
+
+libraryDependencies += "org.creativescala" %% "doodle" % "0.10.1"
---
+++ src/main/scala/spheres-07012022.scala
@@ -0,0 +1,79 @@
+import cats.syntax.all.*
+import cats.effect.unsafe.implicits.global
+import doodle.core.*
+import doodle.random.*
+import doodle.syntax.all.*
+import doodle.java2d.*
+import doodle.interact.syntax.interpolation.*
+
+// Genuary 2022 prompt 1
+// "Draw 10,000 of something."
+object Spheres07012022 {
+  val nLines = 100
+  val nSpheresPerLine = 100
+  val radius = 512
+  val circleMean = 7.0
+  val circleStdDev = 3.5
+  val baseColor = Color.hsla(0.degrees, 1.0, 0.7, 0.8)
+  val colorStdDev = 0.1
+
+  val randomCircle: Random[Picture[Unit]] =
+    Random.normal(circleMean, circleStdDev).map(r => circle(r))
+
+  def randomColor(x: Normalized): Random[Color] = {
+    for {
+      s <- Random.normal(
+        (baseColor.saturation - x).max(0.1),
+        colorStdDev
+      )
+      l <- Random.normal(
+        (baseColor.lightness - x).max(0.1),
+        colorStdDev
+      )
+    } yield baseColor.saturate(s.normalized).lightness(l.normalized)
+  }
+
+  def lineWidth(angle: Angle): Double = Math.abs(angle.cos * radius)
+
+  def line(angle: Angle): Random[Picture[Unit]] = {
+    val width = lineWidth(angle)
+
+    (-width)
+      .upToIncluding(width)
+      .map { x =>
+        val normed = ((x + width) / (width * 2.0)).normalized
+        for {
+          picture <- randomCircle
+          color <- randomColor(normed)
+        } yield picture.strokeColor(color).at(x, 0.0)
+      }
+      .forSteps(nSpheresPerLine)
+      .toList
+      .sequence
+      .map(_.allOn)
+  }
+
+  val lines: Random[Picture[Unit]] = {
+    (-178.0
+      .upToIncluding(178.0)
+      .forSteps(nLines))
+      .map(a => a.degrees)
+      .map(a => line(a).map(_.at(0.0, a.sin * radius)))
+      .toList
+      .sequence
+      .map(_.allOn)
+  }
+
+  val sphere = lines
+
+  @main def spheres070122(): Unit = {
+    import doodle.effect.Writer.*
+
+    val picture = sphere.run
+    val frame = Frame
+      .size(radius * 2 + 40, radius * 2 + 40)
+      .background(Color.hsl(240.degrees, 0.0, 0.0))
+    picture.drawWithFrame(frame)
+    picture.write[Png]("sphere07012022.png", frame)
+  }
+}
